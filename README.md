a3ed1742ecabfa20e7f37c37828a7c45e0ddbe09
Philip Hunkevych <60174953+HunkevychPhilip@users.noreply.github.com>
2020-11-04 10:15:21 +0200

    DATABASE dump added

--- README.md
+++ README.md
@@ -1,7 +1,3 @@
 # Login system skeleton with pure PHP
-To ensure that everything is working properly you need to configure fields listed below (`includes/db.inc.php` file). Also you need to have proper named DATABASE working.
-* $server_name = '`localhost`'; 
-* $db_user_name = '`root`'; 
-* $db_user_password = '`test`'; 
-* $db_name = '`loginSystem`'; 
-
+To ensure that everything is working properly you need to export 
+the DATABASE from `users.sql` file.
---
+++ users.sql
@@ -0,0 +1,68 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.3
+-- https://www.phpmyadmin.net/
+--
+-- Хост: 127.0.0.1
+-- Время создания: Ноя 04 2020 г., 09:03
+-- Версия сервера: 10.4.14-MariaDB
+-- Версия PHP: 7.4.11
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- База данных: `loginsystem`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `username` tinytext NOT NULL,
+  `email` tinytext NOT NULL,
+  `password` longtext NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Дамп данных таблицы `users`
+--
+
+INSERT INTO `users` (`id`, `username`, `email`, `password`) VALUES
+(1, 'test1', 'test1@gm.com', '$2y$10$eqdRhVg6Q3hndiX5vu6LFu.sM0B1oZr1EWET53Qj55zMtT/80mBFy'),
+(2, 'test2', 'test2@gmail.com', '$2y$10$rFJYhgwkU5ElGwiq9P3z5.blGPpXwY3mZNZHQ4dxHpBfVTw1Y.HHm');
+
+--
+-- Индексы сохранённых таблиц
+--
+
+--
+-- Индексы таблицы `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT для сохранённых таблиц
+--
+
+--
+-- AUTO_INCREMENT для таблицы `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
