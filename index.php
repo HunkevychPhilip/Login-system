4276ec7474527c710b322f198a34873c326fa5b5
Philip Hunkevych <60174953+HunkevychPhilip@users.noreply.github.com>
2020-11-04 10:08:29 +0200

    Empty variables handling

--- index.php
+++ index.php
@@ -1,15 +1,45 @@
 <?php require 'header.php'?>
 
-    <main>
-        <div class="container text-center">
-            <?php
-            $message = '';
-            if (isset($_SESSION['username'])) {
-                $message = 'You are logged in.';
-            } else {
-                $message = 'You are logged out.';
+<main class="container mt-5 pt-3 text-center">
+
+    <div class="text-center">
+        <?php
+        $message = '';
+        if (isset($_SESSION['username'])) {
+            $message = 'You are logged in.';
+        } else {
+            $message = 'You are logged out.';
+        }
+        ?>
+        <p><?= $message ?></p>
+
+        <?php
+        if (isset($_GET['error'])) {
+            if ($_GET['error'] == 'empty_fields') {
+                echo '<p style="color: red;">Fields.</p>';
+            } elseif ($_GET['error'] == 'invalid_email_uid') {
+                echo '<p style="color: red;">Email and username.</p>';
+            } elseif ($_GET['error'] == 'invalid_email') {
+                echo '<p style="color: red;">Email.</p>';
+            } elseif ($_GET['error'] == 'invalid_uid') {
+                echo '<p style="color: red;">Username.</p>';
+            } elseif ($_GET['error'] == 'password_check') {
+                echo '<p style="color: red;">Passwords.</p>';
+            } elseif ($_GET['error'] == 'sql_error_init') {
+                echo '<p style="color: red;">Sql stmt error (SELECT username FROM users WHERE username=\'yourUsername\'.</p>';
+            } elseif ($_GET['error'] == 'uid_exists') {
+                echo '<p style="color: red;">Username already taken.</p>';
+            } elseif ($_GET['error'] == 'sql_error_second') {
+                echo '<p style="color: red;">Sql stmt error (INSERT INTO users (username, email, password) VALUES (?, ?, ?);.</p>';
+            } elseif ($_GET['error'] == 'wrong_password') {
+                echo '<p style="color: red;">Wrong password.</p>';
+            }elseif ($_GET['error'] == 'uid_does_not_exist') {
+                echo '<p style="color: red;">There is no such email or username.</p>';
             }
-            ?>
+        } elseif (isset($_GET['sighup']) && $_GET['sighup'] == 'success') {
+            echo '<p style="color: green;">Signup successful!</p>';
+        }
+        ?>
 
             <p><?= $message ?></p>
         </div>
